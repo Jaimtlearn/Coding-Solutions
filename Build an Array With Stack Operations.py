fec0d47312d336057c617db69f077ca38d4e0f10
Jaimit2002 <jaimit.12002813@lpu.in>
2023-11-03 10:24:22 +0530

    Adding Q: Build an Array with stack op

---
+++ Build an Array With Stack Operations.py
@@ -0,0 +1,14 @@
+class Solution
+    def buildArray(self, target List[int], n int) - List[str]
+        ans = []
+        # temp = []
+        for i in range(1,target[-1]+1)
+            # if temp == target
+            #     return ans
+            if i in target
+                ans.append(Push)
+                # temp.append(i)
+            else
+                ans.append(Push)
+                ans.append(Pop)
+        return ans
