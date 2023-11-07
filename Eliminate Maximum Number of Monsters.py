1bdc37d7e9ec05a28694cab3e81de78849e24d4e
Jaimtlearn <jaimitlearn@gmail.com>
2023-11-07 11:25:44 +0530

    Q : Eliminate Maximum number of monsters

---
+++ Eliminate Maximum Number of Monsters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        n = len(dist)
+        time = [dist[i]/speed[i] for i in range(n)]
+        time.sort()
+        print(time)
+        for i in range(n):
+            if time[i] <= i:
+                return i
+
+        return n
